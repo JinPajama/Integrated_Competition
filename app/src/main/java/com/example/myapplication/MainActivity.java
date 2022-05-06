6d361c89e2a4008b3555180db23641538431fff1
JW <kiw970923@naver.com>
2022-05-06 23:37:52 +0900

    테스트 로그 작성

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), UserActivity.class);
                 startActivity(intent);
+                Log.e("test log", "테스트 로그입니다.");
             }
         });
     }
