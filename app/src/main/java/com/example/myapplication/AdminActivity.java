2943def4141efa5531818c47d914f30499209fa2
Jin <jin990208@nate.com>
2022-05-11 01:44:09 +0900

    [ADD] 관리인 Log out

--- app/src/main/java/com/example/myapplication/AdminActivity.java
+++ app/src/main/java/com/example/myapplication/AdminActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_activity);
-    }
-}
+
+        Button button = (Button) findViewById(R.id.btn_logout); /*finish()*/
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+});
+}}
